0d9a7b92909f012ece67d0cbdcdd26721e7a0b44
RASHED <jmrashed@gmail.com>
2024-07-02 01:27:00 +0600

    add number

---
+++ loop.dart
@@ -0,0 +1,8 @@
+void main() {
+  List<int> list = [1, 3, 5, 7, 9, 11, 13, 15, 16, 17, 18, 19, 20];
+  
+  // Using forEach to iterate over the list and print each element
+  list.forEach((i) {
+    print(i);
+  });
+}
